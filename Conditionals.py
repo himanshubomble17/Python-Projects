fd69fc0c620a224b1d250f352e60720584b291bb
Himanshu <himanshubomble91@gmail.com>
2024-02-12 00:29:20 +0530

    Initial commit

---
+++ Conditionals.py
@@ -0,0 +1,94 @@
+# age = int(input("Enter age: "))
+# if (age>=18):
+#     print("You can Drive")
+# else:
+#     print("You can't Drive!")
+
+
+
+# num1 = int(input("Enter number 1: "))
+# num2 = int(input("Enter number 2: "))
+# num3 = int(input("Enter number 3: "))
+# num4 = int(input("Enter number 4: "))
+
+# if (num1>num4):
+#     n1 = num1
+# else:
+#     n1 = num4
+
+# if (num2>num3):
+#     n2 = num2
+# else:
+#     n2 = num3
+
+# if (n1>n2):
+#     print(n1, "is greatest")
+# else:
+#     print(n2, "is greatest")
+
+
+# sub1 = int(input("Enter marks of subject1: "))
+# sub2 = int(input("Enter marks of subject2: "))
+# sub3 = int(input("Enter marks of subject3: "))
+
+# if (sub1<33 or sub2<3 or sub3<33):
+#     print("Failed, you have less than 33% in one of the subject")
+
+# elif(sub1+sub2+sub3)/3 < 40:
+#     print("Failed, due to less total percentage") 
+
+# else:
+#     print("Passed")
+
+
+
+# uname = input("Enter a username: ")
+
+# if (len(uname)<10):
+#     print("Less than 10")
+# else:
+#     print("More than 10")
+
+
+# my_list = ["Himanshu", "Tom", "Brock", "Joe"]
+
+# name = input("Enter a name: ")
+
+# if (name in my_list):
+#     print("Present")
+# else:
+#     print("Absent")
+
+
+# grades = int(input("Enter your Marks: "))
+
+# if (grades>=90):
+#     print("Excellent")
+
+# elif(grades>=80):
+#     print("A")
+
+# elif(grades>=70):
+#     print("B")
+
+# elif(grades>=60):
+#     print("C")
+
+# elif(grades>=50):
+#     print("D")
+    
+# else:
+#     print("F")
+
+
+
+# comment = input("Comment here: ")
+# lower_comment = comment.lower()  # Converting the text input in lower case using .lower() method
+
+# if "harry" in lower_comment:
+#     print("Match found.")
+# else:
+#     print("No match found.")
+
+
+
